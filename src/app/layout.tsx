56978582ae5cb983577f4d0655e4855db2747968
mr_ceh <webmaster.webdeveloper@gmail.com>
2026-05-09 17:14:17 +0330

    fav.icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
 import "./globals.css";
+export const metadata = {
+  icons: {
+    icon: "/fav.png",
+    shortcut: "/fav.png",
+    apple: "/fav.png",
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
