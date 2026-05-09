60be2250659ed1eb2c0dce798ed737465ea53b06
mr_ceh <webmaster.webdeveloper@gmail.com>
2026-05-09 17:02:48 +0330

    fix ltr

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -6,7 +6,7 @@ export default async function Header() {
   return (
     <header className="border-b border-slate-300  bg-(--background) ">
       <div className="mx-auto flex h-12 max-w-6xl items-center justify-between px-4">
-        <Link href="/" className="text-lg font-semibold">
+        <Link href="/" className="text-lg font-semibold" dir="ltr">
           {t("title")}
         </Link>
         <div className="flex items-center gap-2">
